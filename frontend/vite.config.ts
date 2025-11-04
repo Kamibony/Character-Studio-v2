fabfc1085ae3ac85a41b6e82061706da74d75e18
Kamibony <equilibrium.probioticos@gmail.com>
2025-11-03 22:32:08 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,10 +6,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      // Staré endpointy (môžu ostať pre Fázu 0)
       '/getCharacterLibrary': 'http://localhost:8080',
       '/getCharacterById': 'http://localhost:8080',
       '/createCharacterPair': 'http://localhost:8080',
       '/generateCharacterVisualization': 'http://localhost:8080',
+      '/saveVisualization': 'http://localhost:8080',
+
+      // Nové endpointy Fázy 1
+      '/startCharacterTraining': 'http://localhost:8080',
+      '/getTrainedCharacterById': 'http://localhost:8080',
+      '/generateImageFromTrainedCharacter': 'http://localhost:8080',
     }
   }
 })
