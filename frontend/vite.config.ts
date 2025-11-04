8987f765ebd227d45b1e29ff1f9b0ea1f8ed7497
Kamibony <equilibrium.probioticos@gmail.com>
2025-11-04 20:46:28 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <-- PRIDANÉ
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // --- PRIDANÝ BLOK ---
+  build: {
+    // Smeruj výstup o úroveň vyššie a do backend/public
+    outDir: path.resolve(__dirname, '../backend/public'),
+    emptyOutDir: true,
+  },
+  // --------------------
+
   server: {
     proxy: {
       // Staré endpointy (môžu ostať pre Fázu 0)
